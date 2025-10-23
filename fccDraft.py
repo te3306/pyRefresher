d907b548e902e86955b36464e36253f248b0bc04
te3306 <158232992+te3306@users.noreply.github.com>
2025-10-23 14:21:02 -0700

    Update fccDraft.py
    - Building A Game (eg. guessing game).

--- fccDraft.py
+++ fccDraft.py
@@ -774,6 +774,28 @@ while i < 10:
     print(i)
     i += 1
 
+print("Done with loop!")
+
+
+
+
+# Building A Game
+# prompt the user for words.
+
+secret_Word = "giraffe"
+guess = ""
+guess_count = 0
+guess_limit = 3
+out_of_guesses = False
+
+while guess != secret_word and not(out_of_guesses):
+    if guess_count < guess_limit:
+        guess = input("Enter guess: ")
+        guess_count += 1
+    else: 
+        out_of_guesses = True
+
+print("You win!")
 
 
     
